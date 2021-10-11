010c5d3f0170e6dddb4b77810ce96c7815ca0c7c
Anjelica Benitez <55768082+anjelbenitez@users.noreply.github.com>
2021-10-10 17:46:31 -0700

    Delete frontend/utils directory

--- frontend/utils/data_provider.js
+++
@@ -1,9 +0,0 @@
-export async function fetchDataFromServer(tickerId) {
-  try {
-    const res = await fetch(`https://api.marketsseer.com/dev/signals/${tickerId}`);
-    return res.json();
-  } catch (e) {
-    console.error(e);
-    return {};
-  }
-}
